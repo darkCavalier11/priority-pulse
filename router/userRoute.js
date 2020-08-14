4298997247e48b88f41e6e46f8865c9fa8ecaf1f
sumit <sumit.pradhan65@gmail.com>
2020-08-14 22:48:19 +0530

    added router

---
+++ router/userRoute.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const UserModel = require('../database/user')
+
+const app = express()
+const router = new express.Router()
+
+app.use(express.json())
+
+router.post('/add', async(req, res)=>{
+    try{
+        const user = new UserModel(res.body)
+        await user.save()
+        res.status(200).send(user)
+    }
+    catch(err){
+        res.status(400).send({
+            error: err
+        })
+    }
+})
+
+module.exports = router
